954d86f1336e3263e05c000ae83e2ded27595de2
Emmanuella <105435095+OD2022@users.noreply.github.com>
2025-02-06 23:01:23 +0100

    improved is prime

--- index.js
+++ index.js
@@ -13,12 +13,17 @@ const githubUrl = 'https://github.com/OD2022/hng_stage0';
 
 // check if a number is prime
 function isPrime(number) {
-    if (number <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) return false;
-    }
-    return true;
+  // Check if the number is negative or less than 2
+  if (number < 2) return false;
+
+  // Check for factors from 2 up to the square root of the number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) return false;
+  }
+  
+  return true;
 }
+
 
 // check if a number is perfect
 function isPerfect(number) {
